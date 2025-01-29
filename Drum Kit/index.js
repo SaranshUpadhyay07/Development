f11f5064c0552adb9d74f8b33b6487933ad63150
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-01-29 19:48:29 +0530

    Drum kit project

---
+++ Drum Kit/index.js
@@ -0,0 +1,20 @@
+// document.querySelector("button").addEventListener("click", handleClick);//() nahi add karna
+
+// function handleClick(){
+//     alert("I got clicked!");
+// }
+// Or
+for(var i =0;i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function (){
+        
+    });
+    //We called an anonymous function
+}
+
+
+// document.querySelectorAll("button")[0,1,2,3,4,5,6].addEventListener("click", function (){
+//     alert("I got clicked!");
+// }); 
+
+// This will not Work
+
