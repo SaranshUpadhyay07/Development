2f4aa402bd65cc0ea06006cf7ed1dbf406228726
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-01-29 22:14:31 +0530

    Joined Audio with the help of the Switch Statement

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,20 +1,47 @@
-// document.querySelector("button").addEventListener("click", handleClick);//() nahi add karna
 
-// function handleClick(){
-//     alert("I got clicked!");
-// }
-// Or
 for(var i =0;i<document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function (){
-        
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+       
+        var n = this.innerHTML;
+
+        switch(n){
+            case"w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+            break;
+
+            case"a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+            break;
+
+            case"s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+            break;
+
+            case"d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+            break;
+
+            case"j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+            break;
+
+            case"k":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+            break;
+
+            case"l":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+            break;
+
+            default: console.log(n);
+        }
+
     });
-    //We called an anonymous function
 }
-
-
-// document.querySelectorAll("button")[0,1,2,3,4,5,6].addEventListener("click", function (){
-//     alert("I got clicked!");
-// }); 
-
-// This will not Work
-
