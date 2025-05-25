dcd6e190a34f2cff8ed3de70afd0075203005b18
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-05-25 23:30:11 +0530

    Basic Day teller

--- Tute Dude/Backend/EJS/app.js
+++ Tute Dude/Backend/EJS/app.js
@@ -6,7 +6,21 @@ var app = express();
 app.get("/", function(req,res){
     var d = new Date();
     var day = d.getDay();
-    
+    if(day==0){
+        res.send("Day is sunday");
+    }else if(day==1){
+        res.send("Day is monday");
+    }else if(day==2){
+        res.send("Day is Tuesday");
+    }else if(day==3){
+        res.send("Day is wednesday");
+    }else if(day==4){
+        res.send("Day is thursday");
+    }else if(day==5){
+        res.send("Day is friday");
+    }else{
+        res.send("Day is saturday");
+    }
 });
 
 app.listen(3000, function(){
